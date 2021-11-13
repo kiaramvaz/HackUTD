c0e38fcd9b9414d3bdc536c90acfaee98a66bfce
Wilfred Kuate <wilfredkuate@gmail.com>
2021-11-13 14:04:41 -0600

    Firebase Access

---
+++ hackutdreact/src/firestore.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCa-2th1d1eF5xSXJvCbUqh-FWm-xkz-b0",
+  authDomain: "hackutd-c87e3.firebaseapp.com",
+  projectId: "hackutd-c87e3",
+  storageBucket: "hackutd-c87e3.appspot.com",
+  messagingSenderId: "593775232669",
+  appId: "1:593775232669:web:593566094861fec9717674",
+  measurementId: "G-084E362TT1"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
