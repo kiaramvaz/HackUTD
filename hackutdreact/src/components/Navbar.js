93079239d0e9d4711c5b7358b4df0a850fd73b23
Alexander N. Chin <anc200008@utdallas.edu>
2021-11-13 11:54:15 -0600

    This is a lot sorry

---
+++ hackutdreact/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-conatianer">
+
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
