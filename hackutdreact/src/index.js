d7aaebcac95bd4cfadb095ee0f985146687ea1a2
Jesse Musa <jessemusa2@gmail.com>
2021-11-14 00:20:02 -0600

    small things

--- hackutdreact/src/index.js
+++ hackutdreact/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
-import Web from './Backendtest';
+import App from './App';
+//import Web from './Backendtest';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Web />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
